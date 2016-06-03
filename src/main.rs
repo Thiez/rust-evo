d126bc314778cf9d365c15df078e93d5d89a14f3
Matthijs Hofstra <thiezz@gmail.com>
2016-06-03 22:53:09 +0200

    Add iterator for child generation
    Many thanks to durka42, talchas, and rphmeier on #rust for assisting with
    my ridiculous demands :-)

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,14 @@ static AVAILABLE_CHARS: &'static [char] = &[
 ];
 
 fn main() {
-    let target = std::env::args().skip(1).next().unwrap_or("METHINKS IT IS LIKE A WEASEL".into());
-    for c in target.chars().filter(|c|!AVAILABLE_CHARS.contains(c)) {
+    let target: Vec<char> = std::env::args().skip(1).next()
+        .map(|s|s.chars().collect())
+        .unwrap_or("METHINKS IT IS LIKE A WEASEL".chars().collect());
+    for c in target.iter().filter(|c|!AVAILABLE_CHARS.contains(c)) {
         panic!("Bad character: {}, permissable characters: {}", c, AVAILABLE_CHARS.iter().cloned().collect::<String>());
     }
 
-    println!("{}", target);
+    println!("{}", target.iter().cloned().collect::<String>());
 
     let nb_copy = 400;
     let mutation_rate = 0.05f64;
@@ -32,17 +34,15 @@ fn main() {
     }
 
     let mut f_min = std::u32::MAX;
-    let mut sentences: Vec<(u32, String)> = Vec::new();
+    let mut sentences: Vec<(u32, Vec<char>)> = Vec::new();
     while f_min != 0 {
         sentences.clear();
         counter+=1;
 
-        for _ in 0..nb_copy {
-            let sentence = generate_child(&parents, rng);
-            let sentence = mutate(&sentence, mutation_rate, rng);
-            let f = fitness(&target, &sentence);
-            sentences.push((f,sentence));
-        }
+        sentences.extend(reproduction::generate_children(&parents[..], rng.clone())
+            .map(|sentence| mutate(&sentence, mutation_rate, rng))
+            .map(|sentence| (fitness(&target, &sentence), sentence))
+            .take(nb_copy));
 
         sentences.sort_by_key(|tup|tup.0);
         parents.clear();
@@ -51,14 +51,14 @@ fn main() {
         let new_f_min = fitness(&target, best);
         if new_f_min < f_min {
             f_min = new_f_min;
-            println!("{} : {}", best, counter);
+            println!("{} : {}", best.iter().cloned().collect::<String>(), counter);
         }
     }
 }
 
 /// Computes the fitness of a sentence against a target string.
-fn fitness(target: &str, sentence: &str) -> u32 {
-    target.chars().zip(sentence.chars()).map(|(c1, c2)| if c1 != c2 { 1 } else { 0 }).fold(0, |s, n| s + n)
+fn fitness(target: &[char], sentence: &[char]) -> u32 {
+    target.iter().zip(sentence.iter()).filter(|&(&c1, &c2)|c1 != c2).count() as u32
 }
 
 /// Mutation algorithm.
@@ -66,15 +66,15 @@ fn fitness(target: &str, sentence: &str) -> u32 {
 /// It mutates each character of a string, according to a `mutation_rate`.
 /// Please note that for full usefullness, `mutation_rate` should be between
 /// 0 and 1.
-fn mutate<R: Rng>(sentence: &str, mutation_rate: f64, rng: &mut R) -> String {
-    sentence.chars()
-        .map(|c|if mutation_rate < rng.gen_range(0f64, 1.) { c } else { random_char(rng) })
+fn mutate<R: Rng>(sentence: &[char], mutation_rate: f64, rng: &mut R) -> Vec<char> {
+    sentence.iter()
+        .map(|&c|if mutation_rate < rng.gen_range(0f64, 1.) { c } else { random_char(rng) })
         .collect()
 }
 
 /// Generates a random sentence of length `len` from completly random chars.
-fn generate_first_sentence<R: Rng>(len: usize, rng: &mut R) -> String {
-    let mut result = String::new();
+fn generate_first_sentence<R: Rng>(len: usize, rng: &mut R) -> Vec<char> {
+    let mut result = Vec::new();
     for _ in 0..len {
         result.push(random_char(rng));
     }
@@ -87,9 +87,54 @@ fn random_char<R: Rng>(rng: &mut R) -> char {
     AVAILABLE_CHARS[rng.gen_range(0, AVAILABLE_CHARS.len())]
 }
 
-/// Picks two parents and mixes them together.
-fn generate_child<R: Rng>(parents: &[String], rng: &mut R) -> String {
-    let parent1 = rng.choose(parents).expect("at leats one parent");
-    let parent2 = rng.choose(parents).expect("at least one parent");
-    parent1.chars().zip(parent2.chars()).map(|(c1, c2)|*rng.choose(&[c1, c2]).unwrap()).collect()
+mod reproduction {
+    use std::iter::FromIterator;
+    use std::ops::Deref;
+    use rand::Rng;
+
+    pub struct Children<'a, T, R>
+        where
+            &'a T: IntoIterator,
+            <&'a T as IntoIterator>::Item: Deref,
+            <<&'a T as IntoIterator>::Item as Deref>::Target: Clone,
+            T: 'a + FromIterator<<<&'a T as IntoIterator>::Item as Deref>::Target>,
+            R: 'a + Rng,
+    {
+        parents: &'a [T],
+        rng: R,
+    }
+
+    impl<'a, T, R> Iterator for Children<'a, T, R>
+        where
+            &'a T: IntoIterator,
+            <&'a T as IntoIterator>::Item: Deref,
+            <<&'a T as IntoIterator>::Item as Deref>::Target: Clone,
+            T: 'a + FromIterator<<<&'a T as IntoIterator>::Item as Deref>::Target>,
+            R: 'a + Rng,
+    {
+        type Item = T;
+        fn next(&mut self) -> Option<Self::Item> {
+            let parent1 = &self.parents[self.rng.gen_range(0, self.parents.len())];
+            let parent2 = &self.parents[self.rng.gen_range(0, self.parents.len())];
+            let child = parent1.into_iter()
+                .zip(parent2.into_iter())
+                .map(|(e1, e2)|if self.rng.gen() { e1.clone() } else { e2.clone() })
+                .collect();
+            Some(child)
+        }
+    }
+
+    pub fn generate_children<'a, T, R>(parents: &'a [T], rng: R) -> Children<'a, T, R>
+        where
+            &'a T: IntoIterator,
+            <&'a T as IntoIterator>::Item: Deref,
+            <<&'a T as IntoIterator>::Item as Deref>::Target: Clone,
+            T: 'a + FromIterator<<<&'a T as IntoIterator>::Item as Deref>::Target>,
+            R: 'a + Rng,
+    {
+        Children {
+            parents: parents,
+            rng: rng
+        }
+    }
 }
